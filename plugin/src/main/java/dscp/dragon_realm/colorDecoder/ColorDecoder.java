21df44f278def4eaa72d7ab2d205d443f02ecf50
JasperRutten1 <60922940+JasperRutten1@users.noreply.github.com>
2020-10-15 14:09:06 +0200

    colorDecoder name change
    changed package and class name

--- plugin/src/main/java/dscp/dragon_realm/colorDecoder/ColorDecoder.java
+++
@@ -1,9 +0,0 @@
-package dscp.dragon_realm.colorDecoder;
-
-public class ColorDecoder {
-
-    static String decode(String string){
-        String[] splitOnOpenBracket = string.split("[{}]");
-        
-    }
-}
---
+++ plugin/src/main/java/dscp/dragon_realm/stringDecoder/StringDecoder.java
@@ -0,0 +1,11 @@
+package dscp.dragon_realm.stringDecoder;
+
+public class StringDecoder {
+
+    static String colorDecode(String string){
+        StringBuilder sb = new StringBuilder();
+        String[] splitOnOpenBracket = string.split("[{}]");
+        
+        return
+    }
+}
