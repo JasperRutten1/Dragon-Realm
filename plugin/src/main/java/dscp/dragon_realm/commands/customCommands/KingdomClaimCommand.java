4563a8980880f1d00f8bf0745a73fdefa0749bc4
Dylanchill <74691026+Dylanchill@users.noreply.github.com>
2020-11-19 01:26:50 +0000

    Update KingdomClaimCommand.java

--- plugin/src/main/java/dscp/dragon_realm/commands/customCommands/KingdomClaimCommand.java
+++ plugin/src/main/java/dscp/dragon_realm/commands/customCommands/KingdomClaimCommand.java
@@ -22,26 +22,26 @@ public class KingdomClaimCommand extends CustomCommand {
 
     @Override
     public CommandReturn runCommandCode(CommandSender sender, String commandName, String[] args) throws KingdomException, CustomCommandException {
-        if(!(sender instanceof Player)) throw new CustomCommandException("sender must be of type player");
+        if(!(sender instanceof Player)) throw new CustomCommandException("Sender must be of type player.");
         Player player = (Player) sender;
         CommandReturn commandReturn = new CommandReturn(player);
 
         //code
         if(!player.getWorld().getName().equals("world"))
-            throw new CommandException("this command can only be performed in the overworld");
+            throw new CommandException("This command can only be performed in the overworld.");
         Kingdom kingdom = Kingdom.getKingdomFromPlayer(player);
         if(kingdom == null)
-            throw new CommandException("you are not part of a kingdom");
+            throw new CommandException("You are not part of a kingdom.");
         KingdomMember member = kingdom.getMembers().getMember(player);
         assert member != null;
         if(!member.hasPermission(KingdomMemberRank.KNIGHT))
-            throw new CommandException("you need to have the rank of Knight or higher to perform this command");
+            throw new CommandException("You need to have the rank of Knight or higher to perform this command.");
         Chunk chunk = player.getLocation().getChunk();
         if(args.length == 1){
             if(KingdomClaim.isClaimed(chunk.getX(), chunk.getZ()))
-                throw new CommandException("chunk is already claimed");
+                throw new CommandException("Chunk is already claimed.");
             kingdom.getClaim().claimChunk(chunk.getX(), chunk.getZ());
-            commandReturn.addReturnMessage(ChatColor.GREEN + "successfully claimed chunk");
+            commandReturn.addReturnMessage(ChatColor.GREEN + "Successfully claimed chunk.");
         }
         else if(args.length == 2){
             int r = 0;
@@ -50,13 +50,13 @@ public class KingdomClaimCommand extends CustomCommand {
                 r = Integer.parseInt(args[1]);
             }
             catch (NumberFormatException e){
-                throw new CustomCommandException("2nd argument must be a number");
+                throw new CustomCommandException("Second argument must be a number.");
             }
-            if(r > 5) throw new CustomCommandException("radius can only be 5");
+            if(r > 5) throw new CustomCommandException("Radius can only be 5");
             for(Chunk c : KingdomClaim.getChunksInRadius(chunk.getX(), chunk.getZ(), r)){
                 if(kingdom.getClaim().claimChunk(c.getX(), c.getZ())) count++;
             }
-            commandReturn.addReturnMessage(ChatColor.GREEN + "claimed " + count + " chunks");
+            commandReturn.addReturnMessage(ChatColor.GREEN + "Claimed " + count + " Chunks");
         }
 
 
@@ -66,7 +66,7 @@ public class KingdomClaimCommand extends CustomCommand {
 
     @Override
     public String getHelp() {
-        return new CommandHelpGenerator("/kingdom claim", "claim a chunk for your kingdom")
+        return new CommandHelpGenerator("/kingdom claim", "Claim a chunk for your kingdom.")
                 .generateHelp();
     }
 }
