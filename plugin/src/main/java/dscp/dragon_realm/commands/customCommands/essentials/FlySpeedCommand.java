738be60dfeaf8ad06d0c55d4f9f36221e2f6257c
Dylanchill <74691026+Dylanchill@users.noreply.github.com>
2020-11-19 01:46:10 +0000

    Update FlySpeedCommand.java

--- plugin/src/main/java/dscp/dragon_realm/commands/customCommands/essentials/FlySpeedCommand.java
+++ plugin/src/main/java/dscp/dragon_realm/commands/customCommands/essentials/FlySpeedCommand.java
@@ -15,19 +15,19 @@ public class FlySpeedCommand extends CustomCommand {
 
     @Override
     public CommandReturn runCommandCode(CommandSender sender, String commandName, String[] args) throws KingdomException, CustomCommandException {
-        if(!(sender instanceof Player)) throw new CustomCommandException("sender must be of type player");
+        if(!(sender instanceof Player)) throw new CustomCommandException("Sender must be of type player.");
         Player player = (Player) sender;
         CommandReturn commandReturn = new CommandReturn(player);
 
         //code
         try{
             float speed = Float.parseFloat(args[0])/10;
-            if(speed > 10) throw new CustomCommandException("maximum speed is 10");
+            if(speed > 10) throw new CustomCommandException("Maximum speed is 10.");
             player.setFlySpeed(speed);
-            commandReturn.addReturnMessage(ChatColor.AQUA + "set fly speed to " + speed);
+            commandReturn.addReturnMessage(ChatColor.AQUA + "Flight speed successfully set to " + speed);
         }
         catch (NumberFormatException e){
-            throw new CustomCommandException("argument must be number");
+            throw new CustomCommandException("Argument must be a number.");
         }
 
         //return
